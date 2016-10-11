d3215db1a5ad19ae916636a55b349636870a4985
kongzhaolei <zhaoleikong@gmail.com>
2016-10-11 10:36:36 +0800

    扩展sqlconfig.xml,支持多个库

--- src/main/java/org/gradle/needle/util/DBMybatis.java
+++ src/main/java/org/gradle/needle/util/DBMybatis.java
@@ -1,12 +1,34 @@
 /**
- * 
+ *
  */
 package org.gradle.needle.util;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 /**
  * @author kongzhaolei
  *
  */
 public class DBMybatis {
-
+	
+	protected static SqlSessionFactory configssf;
+	protected static SqlSessionFactory datassf;
+	protected static Reader reader;
+	
+	//一个静态方法
+	static
+	{
+		try {
+			reader = Resources.getResourceAsReader("SqlMapConfig.xml");
+			configssf = new SqlSessionFactoryBuilder().build(reader, "configdb");
+			datassf = new SqlSessionFactoryBuilder().build(reader, "datadb");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
