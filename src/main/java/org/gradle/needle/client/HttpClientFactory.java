a0e5132fba67f933570ba494552c5a5e270091ee
kongzhaolei <zhaoleikong@gmail.com>
2017-04-13 10:35:31 +0800

    重大修改，httpclient支持HTTPS SLL

--- src/main/java/org/gradle/needle/client/HttpClientFactory.java
+++ src/main/java/org/gradle/needle/client/HttpClientFactory.java
@@ -5,8 +5,16 @@
 package org.gradle.needle.client;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Map;
+
+import javax.net.ssl.SSLContext;
 
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.http.HttpEntity;
@@ -15,28 +23,87 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
 public class HttpClientFactory {
-	private static CloseableHttpClient sclient;
-	private static Logger logger = Logger.getLogger(HttpClientFactory.class
-			.getName());
+	private static CloseableHttpClient httpclient;
+	private static PoolingHttpClientConnectionManager connManager;
+	private static RequestConfig timeconfig;
+	private static Logger logger = Logger.getLogger(HttpClientFactory.class.getName());
 	private static String response = "";
 	private static int statuscode;
-	// connection和socket超时配置项
-	private static int timeout = 60000;
-	private static RequestConfig timeconfig = RequestConfig.custom()
-			.setSocketTimeout(timeout).setConnectTimeout(timeout).build();
-	
-/**
- * http get and post
- */
-	public static String invokeServiceMethod(String call_type, String url,
-			Map<String, String> header, Map<String, String> body) {
+	private static int timeout = 7000;
+
+	/**
+	 * 初始化配置
+	 */
+	static {
+		// 设置连接池
+		connManager = new PoolingHttpClientConnectionManager();
+		// 设置连接池大小
+		connManager.setMaxTotal(100);
+		connManager.setDefaultMaxPerRoute(connManager.getMaxTotal());
+		// connection和socket超时配置项
+		timeconfig = RequestConfig.custom().setSocketTimeout(timeout).setConnectTimeout(timeout).build();
+	}
+
+	/**
+	 * SSL CloseableHttpClient
+	 * 
+	 * @return CloseableHttpClient
+	 */
+	private static CloseableHttpClient getHttpClient() {
+		RegistryBuilder<ConnectionSocketFactory> registryBuilder = RegistryBuilder.<ConnectionSocketFactory>create();
+		ConnectionSocketFactory plainSF = new PlainConnectionSocketFactory();
+		registryBuilder.register("http", plainSF);
+
+		// 指定信任密钥存储对象和连接套接字工厂
+		try {
+			KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			// 信任任何链接
+			TrustStrategy anyTrustStrategy = new TrustStrategy() {
+				@Override
+				public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+					return true;
+				}
+			};
+			SSLContext sslContext = SSLContexts.custom().useTLS().loadTrustMaterial(trustStore, anyTrustStrategy)
+					.build();
+			LayeredConnectionSocketFactory sslSF = new SSLConnectionSocketFactory(sslContext,
+					SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+			registryBuilder.register("https", sslSF);
+		} catch (KeyStoreException e) {
+			throw new RuntimeException(e);
+		} catch (KeyManagementException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+		Registry<ConnectionSocketFactory> registry = registryBuilder.build();
+		// 设置连接管理器
+		connManager = new PoolingHttpClientConnectionManager(registry);
+		// 构建客户端
+		return HttpClientBuilder.create().setConnectionManager(connManager).build();
+	}
+
+	/**
+	 * http get and post
+	 */
+	public static String invokeServiceMethod(String call_type, String url, Map<String, String> header,
+			Map<String, String> body) {
 
 		if (call_type.equalsIgnoreCase("get")) {
 			response = httpGetWay(url, header);
@@ -50,24 +117,23 @@ public class HttpClientFactory {
 		return response;
 	}
 
-	private static String httpGetWay(String url,
-			Map<String, String> header) {
+	private static String httpGetWay(String url, Map<String, String> header) {
 		// TODO 自动生成的方法存根
 		return null;
 	}
-	
-    /**
-     * 发送POST请求，获取response
-     * HttpPost
-     * @param url
-     * @param header
-     * @param body
-     * @return
-     */
-	private static String httpPostWay(String url,
-			Map<String, String> header, Map<String, String> body) {
-		
-		sclient = HttpClients.createDefault();
+
+	/**
+	 * 发送POST请求，获取response HttpPost
+	 * 
+	 * @param url
+	 * @param header
+	 * @param body
+	 * @return
+	 */
+	private static String httpPostWay(String url, Map<String, String> header, Map<String, String> body) {
+
+		// httpclient = HttpClients.createDefault();
+		httpclient = getHttpClient();
 		HttpPost post = new HttpPost(url);
 		post.setConfig(timeconfig);
 
@@ -82,21 +148,20 @@ public class HttpClientFactory {
 		ArrayList<BasicNameValuePair> pairs = new ArrayList<BasicNameValuePair>();
 		if (body != null) {
 			for (Map.Entry<String, String> entry : body.entrySet()) {
-				pairs.add(new BasicNameValuePair(entry.getKey().trim(), entry
-						.getValue().trim()));
+				pairs.add(new BasicNameValuePair(entry.getKey().trim(), entry.getValue().trim()));
 			}
 		}
 
 		// 发送请求
 		try {
 			post.setEntity(new UrlEncodedFormEntity(pairs, "UTF-8"));
-			HttpResponse httpResponse = sclient.execute(post);
+			HttpResponse httpResponse = httpclient.execute(post);
 			statuscode = httpResponse.getStatusLine().getStatusCode();
 			if (statuscode == HttpStatus.SC_OK) {
 				HttpEntity entity = httpResponse.getEntity();
 				response = EntityUtils.toString(entity, "UTF-8");
 				logger.info("接口请求成功");
-			}else {
+			} else {
 				logger.info("接口请求失败，statuscode： " + statuscode);
 			}
 		} catch (ClientProtocolException e) {
@@ -108,7 +173,7 @@ public class HttpClientFactory {
 		}
 		return response;
 	}
-	
+
 	/*
 	 * 获取响应码
 	 */
