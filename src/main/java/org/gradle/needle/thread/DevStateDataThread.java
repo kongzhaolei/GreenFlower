7ef5256f14c7a717e242b0875d79c637b67a5c68
kongzhaolei <zhaoleikong@gmail.com>
2017-04-21 17:21:03 +0800

    edit6

---
+++ src/main/java/org/gradle/needle/thread/DevStateDataThread.java
@@ -0,0 +1,22 @@
+package org.gradle.needle.thread;
+
+import org.apache.log4j.Logger;
+import org.gradle.needle.client.UDPDataClient;
+
+public class DevStateDataThread implements Runnable {
+	private Logger logger = Logger.getLogger(DevStateDataThread.class.getName());
+
+	@Override
+	public void run() {
+		try {
+			while (UDPDataClient.is_multicast) {
+				Thread.sleep(60000);
+				UDPDataClient.sendDevStateData();
+			}
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage());
+		}
+
+	}
+
+}
