35db3c7636434dc6fa3477d3508a266290b01c4b
kongzhaolei <zhaoleikong@gmail.com>
2017-04-24 17:23:04 +0800

    edit10

--- src/main/java/org/gradle/needle/client/TCPDataClient.java
+++ src/main/java/org/gradle/needle/client/TCPDataClient.java
@@ -1,5 +1,9 @@
 package org.gradle.needle.client;
 
+import org.apache.log4j.Logger;
+import org.gradle.needle.engine.DataGenerator;
+import org.gradle.needle.util.GlobalSettings;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -11,11 +15,15 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
+import jnr.ffi.Struct.dev_t;
 
 public class TCPDataClient {
 
 	private static String HOST;
 	private static int PORT;
+	private static int protocolid = Integer.parseInt(GlobalSettings.getProperty("protocolid"));
+	private static Logger logger = Logger.getLogger(TCPDataClient.class.getName());
+	private static DataGenerator de = new DataGenerator(protocolid);
 
 	public TCPDataClient(String host, int port) {
 		TCPDataClient.HOST = host;
@@ -39,7 +47,8 @@ public class TCPDataClient {
 						}
 					});
 			ChannelFuture future = bs.connect(HOST, PORT).sync();
-			future.channel().writeAndFlush("一个netty客户端的自白");
+			logger.info("已连接到" + HOST + ":" + PORT);
+			future.channel().writeAndFlush(de.genDevWarnLog());
 			future.channel().close().sync();
 		} catch (Exception e) {
 			e.printStackTrace();
