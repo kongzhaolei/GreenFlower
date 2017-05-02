6c0a8a9d85de6f83d0f46e2541545ff7be2d9a0a
kongzhaolei <zhaoleikong@gmail.com>
2017-05-02 16:28:11 +0800

    edit001

--- src/test/java/org/gradle/DataDispGhost.java
+++ src/test/java/org/gradle/DataDispGhost.java
@@ -6,12 +6,10 @@ import org.gradle.needle.util.VTimer;
 public class DataDispGhost {
 
 	private static String host = "10.68.100.61";
-	private static int port = 8080;
+	private static int port = 8804;
 
 	public static void main(String[] args) {
 		VTimer.timerStart();
 		new TCPDataClient(host, port).GeneratorStart();
-
 	}
-
 }
