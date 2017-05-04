8d4034683e2c0fccfcf9cdce80796d8f57d1e98e
kongzhaolei <zhaoleikong@gmail.com>
2017-05-04 18:31:05 +0800

    111

---
+++ src/main/java/org/gradle/needle/model/ModelData.java
@@ -0,0 +1,5 @@
+package org.gradle.needle.model;
+
+public class ModelData {
+
+}
--- src/main/java/org/gradle/needle/util/JsonUtils.java
+++ src/main/java/org/gradle/needle/util/JsonUtils.java
@@ -1,5 +1,10 @@
 package org.gradle.needle.util;
 
+import org.gradle.needle.model.ModelData;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 /**
  * json序列化与反序列化
  * @author kongzhaolei
@@ -11,6 +16,13 @@ public class JsonUtils {
 		
 	}
 	
-	
+	/*
+	 * 嵌套json反序列化
+	 */
+	public void FromJson(String res) {
+		Gson gson = new Gson();
+		ModelData md = gson.fromJson(res,ModelData.class);
+		
+	}
 
 }
