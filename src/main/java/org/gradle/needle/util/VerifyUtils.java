8f4e0cd408fb0d81cdab924dd42fe30a882ca3c1
kongzhaolei <kongzhaolei@10.9.35.190>
2017-05-18 18:28:00 +0800

    123

--- src/main/java/org/gradle/needle/util/VerifyUtils.java
+++ src/main/java/org/gradle/needle/util/VerifyUtils.java
@@ -1,10 +1,14 @@
 package org.gradle.needle.util;
 
 import org.apache.log4j.Logger;
+import org.assertj.db.type.Changes;
+
+import static org.assertj.db.api.Assertions.assertThat;
 
 public class VerifyUtils {
 
 	private static Logger logger = Logger.getLogger(VerifyUtils.class.getName());
+	Changes changes = new Changes();
 
 	public VerifyUtils() {
 
@@ -14,6 +18,11 @@ public class VerifyUtils {
 	 * verify database object
 	 */
 	
+	public static boolean assertDbCreation() {
+		return false;
+		
+		
+	}
 	
 	
 
